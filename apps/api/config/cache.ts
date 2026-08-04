c6293f6e492e657f61e3f9c7b791a5b818e22304
Ale Korhonen <korhaleksi@outlook.com>
2026-08-04 12:16:04 +0300

    Added redis to the stack

---
+++ apps/api/config/cache.ts
@@ -0,0 +1,21 @@
+import { defineConfig, store, drivers } from '@adonisjs/cache'
+import type { InferStores } from '@adonisjs/cache/types'
+
+const cacheConfig = defineConfig({
+  default: 'default',
+
+  stores: {
+    memoryOnly: store().useL1Layer(drivers.memory()),
+
+    default: store()
+      .useL1Layer(drivers.memory())
+      .useL2Layer(drivers.redis({ connectionName: 'main' }))
+      .useBus(drivers.redisBus({ connectionName: 'main' })),
+  },
+})
+
+export default cacheConfig
+
+declare module '@adonisjs/cache/types' {
+  interface CacheStores extends InferStores<typeof cacheConfig> {}
+}
